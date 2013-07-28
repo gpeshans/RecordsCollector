34dd5110ba6ae0eec4b44f70269b2ffc45adf00e
PesanskiGoran <pesanski_goran@yahoo.com>
2013-07-28 21:01:35 +0200

    new 3

--- RecordsCollectorApp/Default.aspx.designer.cs
+++ RecordsCollectorApp/Default.aspx.designer.cs
@@ -12,11 +12,20 @@
 public partial class _Default {
     
     /// <summary>
-    /// ListBox1 control.
+    /// form1 control.
     /// </summary>
     /// <remarks>
     /// Auto-generated field.
     /// To modify move field declaration from designer file to code-behind file.
     /// </remarks>
-    protected global::System.Web.UI.WebControls.ListBox ListBox1;
+    protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    
+    /// <summary>
+    /// Literal1 control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Literal Literal1;
 }
